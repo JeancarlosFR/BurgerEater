c85c83f659f9193a930f6d86d1cf557b96e8a389
Jean Carlos <je.franco@duocuc.cl>
2025-02-19 14:19:30 -0300

    Inicio del proyecto

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+
+
+
+function App() {
+  
+
+  return (
+    <>
+    <body className="bg-yellow-200">
+      
+      <header className=" bg-yellow-400 py-5">
+          <h1 className=" text-center text-3xl  font-black ">BurgerEater</h1>
+        </header>
+
+
+    </body>
+      
+    </>
+  )
+}
+
+export default App
